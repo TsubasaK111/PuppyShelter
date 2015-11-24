41d2d942b58a023ab866819cb0879fd9f4b68772
TsubasaK111 <Tsubasa.K111@gmail.com>
2015-11-25 00:18:49 +0900

    refactor: rewrite youngPups query, still unoperational

--- puppy_queries.py
+++ puppy_queries.py
@@ -1,7 +1,7 @@
 from datetime import date
 from sqlalchemy import Date
+from sqlalchemy import func
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import functions
 from puppy_setup import Base, Shelter, Puppy
@@ -11,7 +11,6 @@ engine = create_engine('sqlite:///puppyShelters.db', echo = True)
 Base.metadata.bind = engine
 DatabaseSession = sessionmaker(bind = engine)
 session = DatabaseSession()
-
 
 # 1. Query all of the puppies and return the results in ascending alphabetical order
 def alphabeticalPuppies():
@@ -24,7 +23,7 @@ def alphabeticalPuppies():
 
 # 2. Query all of the puppies that are less than 6 months old organized by the youngest first
 def youngPups():
-    puppies = session.query(Puppy).filter(Puppy.dateOfBirth > functions.current_date()).order_by(Puppy.dateOfBirth.desc())
+    puppies = session.query(Puppy).filter(Puppy.dateOfBirth > func.date(date.today())).order_by(Puppy.dateOfBirth.desc())
 
     for puppy in puppies:
         print puppy.name
@@ -36,8 +35,18 @@ def youngPups():
 
 # 3. Query all puppies by ascending weight
 def weightedPuppies():
-
+    """ """
 
 # 4. Query all puppies grouped by the shelter in which they are staying
 def shelteredPuppies():
-    
+    """ """
+
+def runAll():
+    alphabeticalPuppies()
+    youngPups()
+    weightedPuppies()
+    shelteredPuppies()
+    print "Success!  All queries run!"
+
+if __name__ == '__main__':
+    runAll()
