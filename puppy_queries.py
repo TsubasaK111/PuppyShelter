38ea114a805ce8322d1cd63692361ba3a075e2bb
TsubasaK111 <Tsubasa.K111@gmail.com>
2015-11-25 00:02:11 +0900

    feat: add query file with one working query out of four planned

---
+++ puppy_queries.py
@@ -0,0 +1,43 @@
+from datetime import date
+from sqlalchemy import Date
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import functions
+from puppy_setup import Base, Shelter, Puppy
+
+engine = create_engine('sqlite:///puppyShelters.db', echo = True)
+
+Base.metadata.bind = engine
+DatabaseSession = sessionmaker(bind = engine)
+session = DatabaseSession()
+
+
+# 1. Query all of the puppies and return the results in ascending alphabetical order
+def alphabeticalPuppies():
+    puppies = session.query(Puppy).order_by(Puppy.name.desc())
+    for puppy in puppies:
+        print puppy.name
+
+    tisToday = str(date.today())
+
+
+# 2. Query all of the puppies that are less than 6 months old organized by the youngest first
+def youngPups():
+    puppies = session.query(Puppy).filter(Puppy.dateOfBirth > functions.current_date()).order_by(Puppy.dateOfBirth.desc())
+
+    for puppy in puppies:
+        print puppy.name
+        print puppy.dateOfBirth
+
+    # print date.today()
+    # print functions.current_date()
+
+
+# 3. Query all puppies by ascending weight
+def weightedPuppies():
+
+
+# 4. Query all puppies grouped by the shelter in which they are staying
+def shelteredPuppies():
+    
