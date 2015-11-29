c33397bab794343e3441a1191de87c6f1ef16aef
TsubasaK111 <Tsubasa.K111@gmail.com>
2015-11-29 15:05:16 +0900

    refactor: attempt current_occupancy functionality with ORM events, non-functional :(

--- puppy_setup.py
+++ puppy_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import (Table, Column, ForeignKey, Integer, String,
                         Date, DateTime, DefaultClause, func)
 from sqlalchemy.sql import select
+
 from sqlalchemy.event import listen
 from sqlalchemy.pool import Pool
 
@@ -14,12 +15,6 @@ association_table = Table('association', Base.metadata,
     Column('shelter_id', Integer, ForeignKey('shelter.id')),
     Column('puppy_id', Integer, ForeignKey('puppy.id'))
 )
-
-
-def my_on_connect(dbapi_con, connection_record):
-    print "New DBAPI connection:", dbapi_con
-
-listen(Pool, 'connect', my_on_connect)
 
 
 class Shelter(Base):
@@ -33,10 +28,6 @@ class Shelter(Base):
     maximum_capacity = Column( Integer )
     current_occupancy = Column( Integer )
     id = Column(Integer, primary_key = True)
-
-
-    # def current_occupancy_counter(self):
-    # return select([func.count()]).where([association.shelter_id == self.id])
 
     # Many to many relationship with Puppy.
     # Puppies can be adopted by one person, or a family of people.
@@ -58,14 +49,14 @@ class Puppy(Base):
     shelter_id = Column( Integer, ForeignKey('shelter.id') )
     id = Column( Integer, primary_key = True )
 
-    current_occupancy_count = select([func.count(puppy.id)]).\
-                              where(puppy.id == self.id).\
-                              limit(1)
-    def current_occupancy_counter(self):
-        shelter.update().\
-        where(shelter.id == self.shelter_id).\
-        values(current_occupancy = current_occupancy_count)
-    current_occupancy_counter()
+    # current_occupancy_count = select([func.count(puppy.id)]).\
+    #                           where(puppy.id == self.id).\
+    #                           limit(1)
+    # def current_occupancy_counter(self):
+    #     shelter.update().\
+    #     where(shelter.id == self.shelter_id).\
+    #     values(current_occupancy = current_occupancy_count)
+    # current_occupancy_counter()
 
     # One to one relationship with Puppy_Profile
     puppy_profile = relationship(
@@ -88,3 +79,31 @@ engine = create_engine('sqlite:///puppyShelters.db', echo = True)
 
 
 Base.metadata.create_all(engine)
+
+
+def my_on_connect(dbapi_con, connection_record):
+    print "New DBAPI connection:", dbapi_con
+
+def puppy_connect(dbapi_con, connection_record):
+    print "New puppy connection:", dbapi_con
+
+def puppy_insert(mapper, connection, target):
+    print "New puppy insert!"
+    print "Mapper is:", mapper
+    print "Connection is:", connection
+    print "Target is:", target
+    print "Target name is:", target.name
+    current_occupancy_count = select([func.count(Puppy.id)]).\
+                              where(Puppy.shelter_id == target.shelter_id).\
+                              limit(1)
+    print current_occupancy_count
+    current_occupancy_counter = shelter.update().\
+                                where(Shelter.id == target.shelter_id).\
+                                values(current_occupancy = current_occupancy_count)
+    print current_occupancy_counter
+    # connection.execute(current_occupancy_counter)
+
+    # print current_occupancy_counter
+listen(Pool, 'connect', my_on_connect)
+
+listen(Puppy, 'after_insert', puppy_insert)
