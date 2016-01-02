2ea161e7cb58ef14779005fabec7179158cf68d9
TsubasaK111 <Tsubasa.K111@gmail.com>
2016-01-02 16:04:04 +0100

    refactor: continue dividing files into packages, pages are not being served(404)

--- puppies/__init__.py
+++ puppies/__init__.py
@@ -1,26 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from puppy_db_setup import Base, Shelter, Puppy, Puppy_Profile, Adopter
-
-import pdb
-import pprint
-
+app = Flask(__name__)
 
 import puppies.views
 
-app = Flask(__name__)
-
-
-engine = create_engine('sqlite:///puppyShelters.db')
-Base.metadata.bind = engine
-DatabaseSession = sessionmaker(bind = engine)
-session = DatabaseSession()
-
-
-if __name__ == "__main__":
-    app.secret_key = "ZUPA_SECRET_KEY!!!"
-    app.debug = True
-    app.run(host = "0.0.0.0", port = 5001)
+app.run(host = "0.0.0.0", port = 5001)
--- runserver.py
+++ runserver.py
@@ -1,2 +1,8 @@
-from yourapplication import app
-app.run(debug=True)
+from puppies import app
+app.run(debug=True, host = "0.0.0.0", port = 5001)
+
+# # Why did we need this if argument again???
+# if __name__ == "__main__":
+#     app.secret_key = "ZUPA_SECRET_KEY!!!"
+#     app.debug = True
+#     app.run(host = "0.0.0.0", port = 5001)
