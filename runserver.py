ee3e93406d5cd86e2f0487f4578b46a51d4210e7
TsubasaK111 <Tsubasa.K111@gmail.com>
2016-01-03 10:27:49 +0100

    refactor: reintroduce if name == main argument for app.run

--- runserver.py
+++ runserver.py
@@ -1,13 +1,7 @@
 from puppies import app
 
 
-app.secret_key = "ZUPA_SECRET_KEY!!!"
-app.debug = True
-app.run(host = "0.0.0.0", port = 5001)
-
-
-# # Why did we need this if argument again???
-# if __name__ == "__main__":
-#     app.secret_key = "ZUPA_SECRET_KEY!!!"
-#     app.debug = True
-#     app.run(host = "0.0.0.0", port = 5001)
+if __name__ == "__main__":
+    app.secret_key = "ZUPA_SECRET_KEY!!!"
+    app.debug = True
+    app.run(host = "0.0.0.0", port = 5001)
