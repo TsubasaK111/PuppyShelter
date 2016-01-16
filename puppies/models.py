6918da3cdfe7d4f4028089ea551b69c5feb469ae
TsubasaK111 <Tsubasa.K111@gmail.com>
2016-01-16 17:36:24 +0900

    test: replace print to console syntax to logger in models.py

--- puppies/models.py
+++ puppies/models.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 
 import pprint
 import pdb
-
+import logging
 
 Base = declarative_base()
 
@@ -116,7 +116,7 @@ def current_occupancy_counter(session, this_shelter_id):
             puppy_count_SQL,
             {"this_shelter_id": this_shelter_id}
         ).scalar()
-    print "puppy_count is: ", puppy_count
+    logging.info( "puppy_count is: ", puppy_count )
     return puppy_count
 
 def remaining_capacity_counter(session, this_shelter_id):
@@ -130,7 +130,7 @@ def remaining_capacity_counter(session, this_shelter_id):
             {"current_occupancy": current_occupancy,
             "shelter_id": this_shelter_id}
         ).scalar()
-    print "remaining capacity is:", remaining_capacity
+    logging.info( "remaining capacity is:", remaining_capacity )
     return remaining_capacity, current_occupancy
 
 def load_balancer(session, this_puppy_id):
@@ -139,8 +139,8 @@ def load_balancer(session, this_puppy_id):
     for shelter in shelters:
         remaining_capacity, ignore = remaining_capacity_counter(session, shelter.id)
         remaining_capacity_list.append(remaining_capacity)
-    print remaining_capacity_list
-    print remaining_capacity_list.index(max(remaining_capacity_list))
+    logging.info( remaining_capacity_list )
+    logging.info( remaining_capacity_list.index(max(remaining_capacity_list)) )
 
 def adopt_puppy(session, puppy_id, adopter_id):
     update_puppy_SQL = text("""
@@ -153,13 +153,13 @@ def adopt_puppy(session, puppy_id, adopter_id):
                                       {"puppy_id": puppy_id,
                                       "adopter_id": adopter_id}
                                          )
-    print "update_puppy_result: ", update_puppy_result
+    logging.info( "update_puppy_result: ", update_puppy_result )
 
 def after_attach(session, instance):
     """ flush session (commit all changes to the SQL db)
         whenever a 'shelter' is added. """
-    print "\nnew attach! instance is: ", instance
-    print "Session.new is: ", session.new
+    logging.info( "\nnew attach! instance is: ", instance )
+    logging.info( "Session.new is: ", session.new )
     if instance.__tablename__ == "shelter":
         session.flush()
 
@@ -169,17 +169,18 @@ event.listen(session, 'after_attach', after_attach)
 
 def before_flush(session, flush_context, instances):
     """populate the "current_occupancy" column, prevent puppy overflows."""
-    print "\nnew flush! Session.new is: ", session.new
+    logging.info( "\nnew flush! Session.new is: ", session.new )
     for each in session.new:
-        print "__tablename__ is: ", each.__tablename__
+        logging.info( "__tablename__ is: ", each.__tablename__ )
         if each.__tablename__ == "puppy":
-            print "each.shelter_id is: ", each.shelter_id
+            logging.info( "each.shelter_id is: ", each.shelter_id )
             remaining_capacity, current_occupancy = remaining_capacity_counter(
                                                             session,
                                                             each.shelter_id)
             if remaining_capacity - 1 < 0:
                 puppies_on_hold.append(each)
-                print "shelter full! puppies_on_hold: ", puppies_on_hold
+                logging.warning( "shelter full! ", each, " will not be inserted." )
+                logging.warning( "puppies_on_hold contains: ", puppies_on_hold )
                 session.expunge(each)
                 pdb.set_trace()
             else:
@@ -191,7 +192,7 @@ def before_flush(session, flush_context, instances):
                     {"current_occupancy": current_occupancy,
                     "shelter_id": each.shelter_id}
                     )
-                print each.name, " has been put into: ", occupancy_update_result
+                logging.info( each.name, " has been put into: ", occupancy_update_result )
 
 
 event.listen(session, 'before_flush', before_flush)
